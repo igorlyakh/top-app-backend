82856dfd23155a9e8bb680dd9a0d986024143fc4
Igor Liakh <igorlyah060@gmail.com>
2024-05-13 22:01:52 +0300

    added mogoose model for review

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ReviewController } from './review.controller';
+import { ReviewSchema } from './review.model';
 
 @Module({
 	controllers: [ReviewController],
+	imports: [
+		MongooseModule.forFeature([
+			{
+				name: 'review',
+				schema: ReviewSchema,
+			},
+		]),
+	],
 })
 export class ReviewModule {}
