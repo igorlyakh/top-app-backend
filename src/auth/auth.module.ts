db10fe32438610628f6869f87d6f3848f9410719
Igor Liakh <igorlyah060@gmail.com>
2024-05-14 20:34:15 +0300

    added user registration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthController } from './auth.controller';
 import { AuthSchema } from './auth.model';
+import { AuthService } from './auth.service';
 
 @Module({
 	controllers: [AuthController],
 	imports: [MongooseModule.forFeature([{ name: 'auth', schema: AuthSchema }])],
+	providers: [AuthService],
 })
 export class AuthModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { hash } from 'bcrypt';
+import { Model } from 'mongoose';
+import { AuthDocument } from './auth.model';
+import { AuthDto } from './dto/auth.dto';
+
+@Injectable()
+export class AuthService {
+	constructor(
+		@InjectModel('auth') private readonly authModel: Model<AuthDocument>,
+	) {}
+
+	async createUser(dto: AuthDto) {
+		const passwordHash = await hash(dto.password, 10);
+		const newUser = { email: dto.login, passwordHash };
+		return this.authModel.create(newUser);
+	}
+
+	async findUser(email: string) {
+		return this.authModel.findOne({ email }).exec();
+	}
+}
