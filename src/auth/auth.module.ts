8351521c949b0e5fd17f332b9b9659cc4f49e52b
Igor Liakh <igorlyah060@gmail.com>
2024-05-18 16:59:03 +0300

    added jwt guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,21 +2,25 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
 import { getJWTConfig } from 'src/configs/jwt.config';
 import { AuthController } from './auth.controller';
 import { AuthSchema } from './auth.model';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
 	controllers: [AuthController],
 	imports: [
 		MongooseModule.forFeature([{ name: 'auth', schema: AuthSchema }]),
+		ConfigModule,
 		JwtModule.registerAsync({
 			imports: [ConfigModule],
 			inject: [ConfigService],
 			useFactory: getJWTConfig,
 		}),
+		PassportModule,
 	],
-	providers: [AuthService],
+	providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
