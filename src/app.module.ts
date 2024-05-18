30550d8350cfabea84e3c6662a102124109f5cb8
Igor Liakh <igorlyah060@gmail.com>
2024-05-18 17:05:56 +0300

    added guard to review/delete

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { getMongoConfig } from './configs/mongo.config';
 import { ProductModule } from './product/product.module';
@@ -22,7 +20,5 @@ import { TopPageModule } from './top-page/top-page.module';
 		ProductModule,
 		ReviewModule,
 	],
-	controllers: [AppController],
-	providers: [AppService],
 })
 export class AppModule {}
