df9555c8daf53f8e7cd4389d0ce55ba1942e1474
Igor Liakh <igorlyah060@gmail.com>
2024-05-24 21:03:29 +0300

    added validation for all routes in top-page & added create, getById services & controller

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -4,20 +4,37 @@ import {
 	Delete,
 	Get,
 	HttpCode,
+	NotFoundException,
 	Param,
 	Patch,
 	Post,
+	UsePipes,
+	ValidationPipe,
 } from '@nestjs/common';
+import { CreateTopPageDto } from './dto/createTopPage.dto';
 import { FindTopPageDto } from './dto/findTopPage.dto';
+import { TOP_PAGE_NOT_FOUND } from './top-page.constants';
 import { TopPageModel } from './top-page.model';
+import { TopPageService } from './top-page.service';
 
 @Controller('top-page')
 export class TopPageController {
+	constructor(private readonly topPageService: TopPageService) {}
+
+	@UsePipes(new ValidationPipe())
 	@Post('create')
-	async create(@Body() dto: Omit<TopPageModel, '_id'>) {}
+	async create(@Body() dto: CreateTopPageDto) {
+		return this.topPageService.create(dto);
+	}
 
 	@Get(':id')
-	async get(@Param('id') id: string) {}
+	async get(@Param('id') id: string) {
+		const topPage = await this.topPageService.getById(id);
+		if (!topPage) {
+			throw new NotFoundException(TOP_PAGE_NOT_FOUND);
+		}
+		return topPage;
+	}
 
 	@Delete(':id')
 	async delete(@Param('id') id: string) {}
@@ -25,6 +42,7 @@ export class TopPageController {
 	@Patch(':id')
 	async patch(@Param('id') id: string, @Body() dto: TopPageModel) {}
 
+	@UsePipes(new ValidationPipe())
 	@HttpCode(200)
 	@Post('find')
 	async find(@Body() dto: FindTopPageDto) {}
--- src/top-page/top-page.service.ts
+++ src/top-page/top-page.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateTopPageDto } from './dto/createTopPage.dto';
+import { TopPageDocument } from './top-page.model';
 
 @Injectable()
-export class TopPageService {}
+export class TopPageService {
+	constructor(
+		@InjectModel('topPage')
+		private readonly topPageModel: Model<TopPageDocument>,
+	) {}
+
+	async create(dto: CreateTopPageDto) {
+		return this.topPageModel.create(dto);
+	}
+
+	async getById(id: string) {
+		return this.topPageModel.findById(id).exec();
+	}
+}
